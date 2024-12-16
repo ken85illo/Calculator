77dc001dcf246be3c2ad18ead8ab11fcb1f5fb4b
ken85illo <pabillokenneth81@gmail.com>
2024-12-16 16:43:30 +0800

    Added Button Enums with Numbers

---
+++ src/Buttons.java
@@ -0,0 +1,18 @@
+enum Functions {
+    ALL_CLEAR(0), DELETE(1), MODULUS(2), PLUS(3), 
+    NUMBER_7(4), NUMBER_8(5), NUMBER_9(6), MINUS(7),
+    NUMBER_4(8), NUMBER_5(9), NUMBER_6(10), TIMES(11),
+    NUMBER_3(12), NUMBER_2(13), NUMBER_1(14), DIVIDE(15),
+    NUMBER_0(16), POINT(17), EQUALS(18);
+
+    int number;
+
+    Functions(int number) {
+
+    }
+}
+
+public class Buttons {
+
+    
+}
